88218e5ffb70748308f1aea77fc3dfd07b9dd36b
MuhammadZaini <32381402+MuhammadZaini7@users.noreply.github.com>
2017-10-22 23:31:46 -0700

    Menghitung dan Menampilkan Total Biaya

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -9,9 +9,11 @@ int main (){
 	cout << "Luas 1 box parket      : "<<luas1Box<<" m2"<<endl;
 	double jumlahBoxP,biayaP,biayaPG,totalB;
 	jumlahBoxP = luasRumah/luas1Box;
-	cout <<"Jumlah box parket 		: "<<jumlahBoxP<<endl;
+	cout <<"Jumlah box parket       : "<<jumlahBoxP<<endl;
 	biayaP = jumlahBoxP * 500;
-	cout << "Biaya Parket (Rp)  	: "<< biayaP<<endl;
+	cout << "Biaya Parket (Rp)      : "<< biayaP<<endl;
 	biayaPG = luasRumah * 20;
-	cout <<"Biaya Pasangan (Rp) 	: "<< biayaPG<<endl;
+	cout <<"Biaya Pasangan (Rp)     : "<< biayaPG<<endl;
+	totalB = biayaP+biayaPG;
+	cout << "Total Biaya (Rp)       : "<< totalB<<endl;
 }
